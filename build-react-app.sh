69a611fe7ef0625e6782d32b699a88d1d2c8d925
haijun.zhang <haijun.zhang@joymo.tech>
2021-02-23 21:19:55 +0800

    Initial commit

---
+++ build-react-app.sh
@@ -0,0 +1,10 @@
+#https://www.html.cn/create-react-app/docs/getting-started/
+# 安装react
+cnpm install -g create-react-app
+# 使用 create-react-app 快速构建 React 开发环境 貌似要翻墙~~~
+npx create-react-app react-router-example
+
+npm config set registry https://registry.npm.taobao.org
+#-- 配置后可通过下面方式来验证是否成功
+npm config get registry
+#-- 或 npm info express
